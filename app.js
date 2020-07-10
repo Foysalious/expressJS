464f3eb35413de5e3a29efb00c98c29e27f91a2c
foysalious <foysalrahman112@gmail.com>
2020-07-10 19:18:03 +0600

    Added BUG

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require ('express');
 const path =  require('path');
 const mongoose =  require('mongoose');
+const bodyParser= require('body-parser');
+const config = require('./config/database');
 
-mongoose.connect('mongodb://localhost/nodekb');
+
+mongoose.connect(config.database);
 let db = mongoose.connection;
 
 // Check connection
-db.once('open',function(){
+db.once('open', function(){
   console.log('Connected to MongoDB');
 });
 
-//Check for DB error
-db.on('error',function(err){
+// Check for DB errors
+db.on('error', function(err){
   console.log(err);
 });
 //Init App
@@ -23,6 +26,12 @@ let Article = require('./models/article');
 //Load View Engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
+
+// Body parser Middleware parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //Home Route
 app.get('/',function(req,res){
@@ -38,12 +47,32 @@ app.get('/',function(req,res){
   });
 });
 
+
 //Add Route
 app.get('/articles/add',function(req,res){
-  res.render('add',{
+  res.render('add_article',{
     title:'Add Articles'
   });
 });
+
+//Add Submit POST Route
+app.post('/articles/add',function(req,res){
+  let article = new Article();
+  article.title= req.body.title;
+  article.author= req.body.author;
+  article.body= req.body.body;
+
+  article.save(function(err){
+    if (err) {
+      console.log(err);
+      return;
+    }else {
+        res.redirect('/');
+      }
+  });
+
+});
+
 
 //Start Server
 app.listen(3000,function(){
